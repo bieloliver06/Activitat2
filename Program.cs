88f073db3bb49e1301415981ebab6df2e7c0bc7a
BuildTools <unconfigured@null.spigotmc.org>
2022-11-07 11:50:06 +0100

    null

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿namespace Activitat2
+{
+    class Program
+    {
+        static void Main()
+        {
+            Console.WriteLine("Introdueix el teu nom : ");
+            string? name = Console.ReadLine();
+            Console.WriteLine($"Benvolgut {name}!");
+        }
+    }
+}
